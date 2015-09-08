fb5fc8088021f06d35764ad4bc2f24272008a58f
Michael Lam <mlam@astro.cornell.edu>
2015-09-08 00:30:44 -0400

    major update of acf/scintillation param code

--- pypulse/dynamicspectrum.py
+++ pypulse/dynamicspectrum.py
@@ -8,8 +8,13 @@ To do: grid: set clim such that 0 is white, not blue,mask data,add zorder
 import numpy as np
 import numpy.ma as ma
 import utils as u
+import functionfit as ffit
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from scipy.signal import fftconvolve
+import scipy.optimize as optimize
+
+
 
 class DynamicSpectrum:
     def __init__(self,data,offdata=None,errdata=None,mask=None,F=None,T=None,extras=None,verbose=True):
@@ -41,6 +46,10 @@ class DynamicSpectrum:
             else:
                 self.extras = extras
 
+        # Pre-define variables
+        self.acf = None
+                
+
     def getValue(self,f,t,df=1,dt=1,err=False,index=False):
         '''
         Returns value of dynamic spectrum
@@ -66,19 +75,138 @@ class DynamicSpectrum:
                     total+=data[indF,indT]
                     N+=1
             return total/float(N)
+
+
+    def remove_baseline(self,function="gaussian"):
+        """
+        Attempts to remove the baseline amplitude from the dynamic spectrum
+        """
+        flatdata = self.data.flatten()
+        interval = np.power(10,np.floor(np.log10(np.ptp(flatdata/100)))) #~100 divisions, but bins to an even power of 10
+        center,hist = u.histogram(flatdata,interval=interval)
+
+        if function == "gaussian":
+            p1,err = ffit.gaussianfit(center,hist)
+            y = ffit.funcgaussian(p1,center)
+            peak = center[np.argmax(y)]
+            
+        elif function == "simple_DISS":
+            area = np.trapz(hist,x=center)
+            shift = -np.min(center)+1.0
+            x = center + shift
+            y = np.array(hist,dtype=np.float)/area
+            p1,err = ffit.simpleDISSpdffit(x,y)
+            y1 = ffit.funcsimpleDISSpdf(p1,x)*area
+            peak = center[np.argmax(y1)]
+        else:
+            peak = 0.0
+
+        self.data -= peak
+        return self
         
-    def acf2d(self,speed='fast',mode='full'):
+    def acf2d(self,remove_baseline=True,speed='fast',mode='full'):
         """
         Calculate the two-dimensional auto-correlation function of the dynamic spectrum
         """
-        return u.acf2d(self.data,speed=speed,mode=mode) #do more here
+        if remove_baseline:
+            self.remove_baseline()
+
+        # Have if statement to apply mask: set ones in the norm to 0.0
+
+        ones = np.ones(np.shape(self.data))
+        norm = fftconvolve(ones,np.flipud(np.fliplr(ones)),mode=mode)
+        acf = fftconvolve(self.data,np.flipud(np.fliplr(self.data)),mode=mode)/norm
 
 
-    def scintillation_parameters(self):
+        # Replace the central noise spike with that of the next highest of its neighbors
+        acfshape = np.shape(acf)
+        centerrind = acfshape[0]/2
+        centercind = acfshape[1]/2
+        acf[centerrind,centercind] = 0.0
+        acf[centerrind,centercind] = np.max(acf[centerrind-1:centerrind+2,centercind-1:centercind+2])
+
+        self.acf = acf
+        return acf
+            
+            
+
+        #return u.acf2d(self.data,speed=speed,mode=mode) #do more here
+
+
+    def scintillation_parameters(self,plotbound=1.0):
+        if self.acf is None:
+            self.acf2d()
+
+
+        acfshape = np.shape(self.acf)
+        centerrind = acfshape[0]/2
+        centercind = acfshape[1]/2
+            
+        # Look for the central peak in the ACF
+
+        MIN = np.min(self.acf) 
+        if MIN < 0: #The min value is approximately from a gaussian distribution
+            MIN = np.abs(MIN)
+        else:
+            #center,hist = u.histogram(acf.flatten(),interval=0.001) #relies on 0.001
+            #MIN = center[np.argmax(hist)]
+            MIN = u.RMS(self.acf.flatten())
+        rslice = self.acf[centerrind:,centercind]
+        maxr = np.where(rslice<=MIN)[0][0]
+        cslice = self.acf[centerrind,centercind:]
+        maxc = np.where(cslice<=MIN)[0][0]
+
+        plotacf = self.acf[centerrind-plotbound*maxr+1:centerrind+plotbound*maxr,centercind-plotbound*maxc+1:centercind+plotbound*maxc+1]
+
         
-        pass
+        params, pcov = ffit.fitgaussian2d(plotacf)
 
+        fit = ffit.gaussian2d(*params)
+        amplitude,center_x,center_y,width_x,width_y,rotation,baseline = params
+        if self.verbose:
+            paramnames = ["amplitude","center_x","center_y","width_x","width_y","rotation","baseline"]
+            if pcov is not None:
+                paramerrors = np.array(map(np.sqrt,np.diagonal(pcov))) #multiply by s_sq!!!!!
+            else:
+                paramerrors = np.zeros_like(params)
+            for i,param in enumerate(params):
+                print "%s: %0.2e+/-%0.2e"%(paramnames[i],param,paramerrors[i])
+                
 
+        #Solve for scintillation parameters numerically
+        SHAPE = np.shape(plotacf)
+        
+        try:
+            delta_t_d = (optimize.brentq(lambda y: fit(SHAPE[0]/2,y)-baseline-amplitude/2.0,(SHAPE[1]-1)/2,SHAPE[1]*2)-(SHAPE[1]-1)/2) #FWHM test
+            print "delta_t_d %0.3f minutes"%(np.sqrt(2)*(delta_t_d/(2*np.sqrt(2*np.log(2)))))
+        except ValueError:
+            print "ERROR in delta_t_d"
+            delta_t_d = SHAPE[1]
+
+        try:
+            delta_nu_d = (optimize.brentq(lambda x: fit(x,SHAPE[1]/2)-baseline-amplitude/2.0,(SHAPE[0]-1)/2,SHAPE[0])-(SHAPE[0]-1)/2)
+            print "delta_nu_d %0.3f MHz"%(np.sqrt(2)*(delta_nu_d/(2*np.sqrt(2*np.log(2)))))
+        except ValueError:
+            print "ERROR in delta_nu_d"
+            delta_nu_d = SHAPE[0]
+
+        print "dnu/dt %0.3f MHz/min" % (np.tan(rotation))#((dF/dT)*np.tan(rotation))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(211)
+        u.imshow(self.data)
+        ax = fig.add_subplot(212)
+
+        u.imshow(plotacf)
+        plt.colorbar()
+        levels = (amplitude*np.array([1.0,0.5,1.0/np.e]))+baseline
+        levels = (amplitude*np.array([0.5]))+baseline
+        print levels
+
+        ax.contour(fit(*np.indices(plotacf.shape)),levels, colors='k')
+        #ax.set_xlim(len(xs)-20,len(xs)+20)
+        #ax.set_ylim(len(ys)-10,len(ys)+10)
+        plt.show()
 
 
 
