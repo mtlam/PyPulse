543c631c088b267871b5f926e1f1495623a3012b
Michael Lam <mlam@astro.cornell.edu>
2015-09-16 01:38:38 -0400

    updated with tim

--- pypulse/__init__.py
+++ pypulse/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ["archive","Archive","singlepulse","SinglePulse","dynamicspectrum","DynamicSpectrum"]
+__all__ = ["archive","Archive","singlepulse","SinglePulse","dynamicspectrum","DynamicSpectrum","par","Par","tim","Tim","TOA"]
 
 from archive import Archive
 from singlepulse import SinglePulse
 from dynamicspectrum import DynamicSpectrum
+from par import Par
+from tim import Tim,TOA
