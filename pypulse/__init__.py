353be3ab91e952309cd10323816335d0c3a21d8c
mtlam <mtlam@github.com>
2015-10-13 15:23:26 -0400

    correct imports

--- pypulse/__init__.py
+++ pypulse/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ["archive","Archive","singlepulse","SinglePulse","dynamicspectrum","DynamicSpectrum","par","Par","tim","Tim","TOA"]
 
-from archive import Archive
-from singlepulse import SinglePulse
-from dynamicspectrum import DynamicSpectrum
-from par import Par
-from tim import Tim,TOA
+from pypulse.archive import Archive
+from pypulse.singlepulse import SinglePulse
+from pypulse.dynamicspectrum import DynamicSpectrum
+from pypulse.par import Par
+from pypulse.tim import Tim,TOA
