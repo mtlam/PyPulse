fed169d5d916e15df1a7e0b120a0f9888e1cd179
mtlam <mtlam@github.com>
2016-05-03 02:03:55 -0400

    added getFD()

--- pypulse/par.py
+++ pypulse/par.py
@@ -201,15 +201,17 @@ class Par:
         DM = self.getDM()
         return ts,dmxs + float(DM), errs #float from possible decimal!
 
-    def getFDfunc(self):
-        """
-        Returns a function that provides the timing delays as a function of observing frequency
-        """
+    def getFD(self):
         coeffs = []
         for param in self.paramlist:
             if "FD" in param:
                 coeffs.append(self.get(param))
-        f = lambda nu: -1e6*np.polyval(coeffs[::-1],np.log(nu)) #nu in GHz, returns values in microseconds
+        return np.array(coeffs)
+    def getFDfunc(self):
+        """
+        Returns a function that provides the timing delays as a function of observing frequency
+        """
+        f = lambda nu: -1e6*np.polyval(self.getFD()[::-1],np.log(nu)) #nu in GHz, returns values in microseconds
         return f
 
     def getName(self):
