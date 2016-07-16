b6197028ea610b6fbd585b975379b1010440c9ba
mtlam <mtlam@github.com>
2016-07-16 14:34:33 -0400

    start calibrator class

---
+++ pypulse/calibrator.py
@@ -0,0 +1,16 @@
+'''
+Michael Lam 2016
+
+Loads a PSRFITS calibrator
+
+EXAMPLE USAGE:
+ar = Archive(filename)
+ar.tscrunch()
+ar.pavplot()
+
+'''
+
+
+class Calibrator:
+    def __init__(self,filename):
+        pass
