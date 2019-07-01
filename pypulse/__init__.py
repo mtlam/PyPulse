21087aabbcaa48d3e7c71c86774a7ed5d5cf49ae
mtlam <mtlam@github.com>
2019-06-30 21:22:53 -0400

    pylint improvement +17.5

--- pypulse/__init__.py
+++ pypulse/__init__.py
@@ -1,4 +1,6 @@
-__all__ = ["archive","Archive","singlepulse","SinglePulse","dynamicspectrum","DynamicSpectrum","par","Par","Parameter","tim","Tim","TOA","utils"]
+__all__ = ["archive", "Archive", "singlepulse", "SinglePulse",
+           "dynamicspectrum", "DynamicSpectrum", "par", "Par",
+           "Parameter", "tim", "Tim", "TOA", "utils"]
 
 __version__ = 0.1
 
@@ -6,5 +8,5 @@ from pypulse.archive import Archive
 from pypulse.singlepulse import SinglePulse
 from pypulse.dynamicspectrum import DynamicSpectrum
 from pypulse.par import Par
-from pypulse.tim import Tim,TOA
-from pypulse.dmx import DMX,DM
+from pypulse.tim import Tim, TOA
+from pypulse.dmx import DMX, DM
