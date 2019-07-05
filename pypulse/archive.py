9b6da583dde6e84605544acbd13fcbb4c8d67137
mtlam <mtlam@github.com>
2019-07-05 17:40:50 -0400

    Ross: Cache data and weighted data

--- pypulse/archive.py
+++ pypulse/archive.py
@@ -281,13 +281,13 @@ class Archive(object):
             '''
 
             tf = tempfile.NamedTemporaryFile()
-            self.data = np.memmap(tf.name,
+            self._data = np.memmap(tf.name,
                                    dtype=np.float32,
                                    mode='w+',
                                    shape=(nsubint, npol, nchan, nbin))
 
         else:
-            self.data = np.zeros((nsubint, npol, nchan, nbin))
+            self._data = np.zeros((nsubint, npol, nchan, nbin))
 
         #self.data = np.zeros((nsubint, npol, nchan, nbin))
         #data = np.zeros((nsubint, npol, nchan, nbin))
@@ -303,16 +303,16 @@ class Archive(object):
 
 
         if nsubint == 1 and npol == 1 and nchan == 1:
-            self.data = (DAT_SCL*DATA+DAT_OFFS)#*DAT_WTS
+            self._data = (DAT_SCL*DATA+DAT_OFFS)#*DAT_WTS
         elif nsubint == 1 and npol == 1:
             for k in K:
-                self.data[0, 0, k, :] = (DAT_SCL[0, k]*DATA[0, 0, k, :]+DAT_OFFS[0, k])#*DAT_WTS[0, k] #dat WTS[0]?
+                self._data[0, 0, k, :] = (DAT_SCL[0, k]*DATA[0, 0, k, :]+DAT_OFFS[0, k])#*DAT_WTS[0, k] #dat WTS[0]?
         elif nsubint == 1 and nchan == 1:              
             for j in J:
-                self.data[0, j, 0, :] = (DAT_SCL[0, j]*DATA[0, j, 0, :]+DAT_OFFS[0, j])#*DAT_WTS[0]
+                self._data[0, j, 0, :] = (DAT_SCL[0, j]*DATA[0, j, 0, :]+DAT_OFFS[0, j])#*DAT_WTS[0]
         elif npol == 1 and nchan == 1:
             for i in I:
-                self.data[i, 0, 0, :] = (DAT_SCL[i, 0]*DATA[i, 0, 0, :]+DAT_OFFS[i, 0])#*DAT_WTS[0]
+                self._data[i, 0, 0, :] = (DAT_SCL[i, 0]*DATA[i, 0, 0, :]+DAT_OFFS[i, 0])#*DAT_WTS[0]
         else: #if nsubint == 1 or npol == 1 or nchan == 1 this works,  or all three are not 1, might want to split this up
             t0 = time.time()
             cudasuccess = False
@@ -406,11 +406,9 @@ class Archive(object):
                 scale = DAT_SCL.reshape(nsub, npol, nchan)
                 offset = DAT_OFFS.reshape(nsub, npol, nchan)
                 weights = DAT_WTS.reshape(nsub, 1, nchan, 1)
-                self.data = (scale*DATA.transpose((3, 0, 1, 2)) + offset).transpose((1, 2, 3, 0))
+                self._data = (scale*DATA.transpose((3, 0, 1, 2)) + offset).transpose((1, 2, 3, 0))
             t1 = time.time()
-            #print t1-t0
-
-
+        self.data = self._data
 
         bw = self.getBandwidth()
 
@@ -1063,26 +1061,19 @@ class Archive(object):
         # Apply calibrations
 
 
+    @property
+    def data(self):
+        return self._data
 
-
-
-
-
-
+    @data.setter
+    def data(self, value):
+        self._data = value
+        self.weighted_data = value*self.weights[:,None,:,None]
 
     def getData(self, squeeze=True, setnan=None, weight=True):
         """Returns the data array,  fully squeezed"""
         if weight:
-            data = np.zeros_like(self.data)
-            I, J, K, L = np.shape(self.data)
-            I = range(I)
-            J = range(J)
-            K = range(K)
-            for i in I:
-                #print np.shape(self.weights), np.shape(data), np.shape(self.data), i#, j, k
-                for j in J:
-                    for k in K:
-                        data[i, j, k, :] = self.data[i, j, k, :]*self.weights[i, k]
+            data = self.weighted_data
         else:
             data = self.data
 
@@ -1091,8 +1082,10 @@ class Archive(object):
 
         if setnan is not None:
             data = np.where(data == setnan, np.nan, data)
-
-        return np.copy(data) #removes pointer to data
+        
+        #return np.copy(data) #removes pointer to data
+        return data
+    
     def setData(self, newdata):
         """Sets the data,  very dangerous!"""
         self.record(inspect.currentframe())
