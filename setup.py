b5787a523e089fe9029de62af6c0ca2ca57ca299
Michael Lam <astromlam@gmail.com>
2020-03-01 14:10:40 -0500

    update setups

--- setup.py
+++ setup.py
@@ -12,6 +12,12 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+
 setup(
     name='PyPulse',
     version=pypulse.__version__,
@@ -24,12 +30,14 @@ setup(
     zip_safe=False,
     license='BSD-3',
     description='A python package for handling and analyzing PSRFITS files.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numpy', 'scipy', 'matplotlib', 'astropy'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD-3 Clause License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ]
