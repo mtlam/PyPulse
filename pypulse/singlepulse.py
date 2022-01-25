component_fitting() now outputs paas type text file for components. Simplified von Mises function to use scipy's now, fixing an underlying calculation with kappa

pypulse/singlepulse.py
@@ -207,8 +207,8 @@ class SinglePulse(object):
         return self.opw
 
     def calcFT(self):
-        """ 
-        Calculate the Fourier transform and frequencies 
+        """
+        Calculate the Fourier transform and frequencies
         Useful for pre-calculating a template FT
         """
         self.yfft = np.fft.rfft(self.data)
@@ -293,7 +293,7 @@ class SinglePulse(object):
     def fitPulse(self, template, fixedphase=False, rms_baseline=None, remove_baseline=True):
         """
         Returns tauccf, tauhat, bhat, sigma_Tau, sigma_b, snr, rho
-        """                
+        """
         if self.null or len(template) != self.getNbins():
             return None
         #if rms_baseline is None:
@@ -524,7 +524,7 @@ class SinglePulse(object):
     def component_fitting(self, mode='gaussian', nmax=10, full=False,
                           minamp=None, alpha=0.05, allownegative=False,
                           verbose=False, plot=False, save=False,
-                          filename=None):
+                          filename=None, paasfilename=None):
         '''
         Fitting to phases is much more numerically stable for von mises function
         '''
@@ -555,7 +555,9 @@ class SinglePulse(object):
                 if filename is not None:
                     plt.savefig(filename)
                 plt.show()
-                
+
+            if paasfilename is not None and mode == 'vonmises':
+                u.paasoutput(pfit, filename=paasfilename)
             if save:
                 self.data = fitfunc(pfit, self.phases)
             if full:
@@ -736,21 +738,21 @@ class SinglePulse(object):
 
 
     def addNoise(self, sn, save=False):
-        """ 
-        Add noise to the pulse such that it will roughly 
-        have an S/N given by sn 
         """
-        
+        Add noise to the pulse such that it will roughly
+        have an S/N given by sn
+        """
+
         minimum = np.mean(self.getOffpulse())
         maximum = np.max(self.data)
         amplitude = maximum - minimum
         sigma = amplitude/sn
-        
+
         retval = self.data + np.random.normal(0, sigma, self.nbins)
         if save:
             self.data = retval
         return retval
-        
+
 
     def getPeriod(self):
         """ Return the pulse period """

pypulse/utils.py
@@ -552,21 +552,30 @@ def gaussian(x, amp, mu, sigma):
     return amp*np.exp(-0.5*((x-mu)/sigma)**2)
 
 def vonmises(x, amp, mu, kappa):
-    #return amp*np.exp(kappa*np.cos(x-mu))/(2*np.pi*special.iv(0, kappa))
     '''
-    # More numerically stable:
-    ive(v, z) = iv(v, z) * exp(-abs(z.real)), z here must be positive number here
-    therefore
-    iv(v, z) = ive(v, z) / exp(-z)
-    log(iv(v,z)) = log(ive(v, z) / exp(-z)) = log(ive(v,z)) - log(exp(-z)) = log(ive(v,z)) + z
+    von Mises component, utilizing scipy's vonmises support.
     '''
-    numer = kappa*np.cos(x-mu)
-    denom = np.log(2*np.pi) + np.log(special.ive(0, kappa)) + kappa
-    y = np.exp(numer - denom)
-    #y /= np.max(y)
-    # Allow for negatives
-    y /= np.max(np.abs(y))
+    y = stats.vonmises.pdf(2*np.pi*(x-mu), kappa)
+    y /= np.max(np.abs(y)) #force positive components
     return amp*y
+
+
+def paasnumber(num):
+    return "%.6s"% ("%0.6f"%num)
+def paasoutput(params, filename="paas.m"):
+    '''
+    Output a file that can be read in by PSRCHIVE's paas program
+    Each line is a von Mises component, with three parameters:
+        location, kappa, amplitude
+    where amplitude is multiplied by the normalized template height
+    '''
+    output = ""
+    for i in range(0, len(params), 3):
+        output += "      %s       %s       %s\n"%(paasnumber(params[i+1]), paasnumber(params[i+2]), paasnumber(params[i]))
+    with open(filename, 'w') as FILE:
+        FILE.write(output)
+
+
 
 def pbf_clean(t, y, g=None, taud=1.0, opw=None, gamma=0.05, m=1.0, x=1.5, stop=1.5):
     '''